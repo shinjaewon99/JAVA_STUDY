82d7b1b9d93ab7f4c2a813b235b88e4bfa4ab9c4
shinjaewon99 <tlswodnjs99@naver.com>
2024-04-23 22:40:57 +0900

    feat : 함수형 프로그래밍 적용

--- src/main/java/ch2/OOPExam.java
+++ src/main/java/ch2/OOPExam.java
@@ -2,33 +2,73 @@ package ch2;
 
 public class OOPExam {
     public static void main(String[] args) {
-        final CalculateService calculateService = new CalculateService();
-        final int addition = calculateService.sum('+', 1, 2);
+        final CalculateService calculateService = new CalculateService(new Addition());
+        final int addition = calculateService.calculate(1, 2);
         System.out.println(addition);
 
-        final int subtraction = calculateService.sum('-', 1, 2);
+        final int subtraction = calculateService.calculate(1, 2);
         System.out.println(subtraction);
 
-        final int multiplication = calculateService.sum('*', 1, 2);
+        final int multiplication = calculateService.calculate(1, 2);
         System.out.println(multiplication);
 
-        final int division = calculateService.sum('/', 8, 2);
+        final int division = calculateService.calculate(8, 2);
         System.out.println(division);
+
+        // 행동 자체인 객체를 파라미터로 넘긴다.
+        final FpCalculatorService fpCalculatorService = new FpCalculatorService();
+        System.out.println("addition : " + fpCalculatorService.calculate(new Addition(), 1, 2));
+        System.out.println("subtraction : " + fpCalculatorService.calculate(new Subtraction(), 3, 2));
+    }
+}
+
+interface Calculation {
+    int calculate(int first, int second);
+}
+
+class Addition implements Calculation {
+    @Override
+    public int calculate(int first, int second) {
+        return first + second;
+    }
+}
+
+class Subtraction implements Calculation {
+    @Override
+    public int calculate(int first, int second) {
+        return first - second;
+    }
+}
+
+class Mutilation implements Calculation {
+    @Override
+    public int calculate(int first, int second) {
+        return first * second;
+    }
+}
+
+class Division implements Calculation {
+    @Override
+    public int calculate(int first, int second) {
+        return first / second;
     }
 }
 
 class CalculateService {
-    public int sum(final char calculation, final int first, final int second) {
-        if (calculation == '+') {
-            return first + second;
-        } else if (calculation == '-') {
-            return first - second;
-        } else if (calculation == '*') {
-            return first * second;
-        } else if (calculation == '/') {
-            return first / second;
-        } else {
-            throw new IllegalArgumentException();
-        }
+    private final Calculation calculation;
+
+    public CalculateService(final Calculation calculation) {
+        this.calculation = calculation;
+    }
+
+    public int calculate(int first, int second) {
+        return calculation.calculate(first, second);
+    }
+}
+
+class FpCalculatorService {
+    public int calculate(Calculation calculation, int first, int second) {
+        // 함수형 프로그래밍, Calculation을 파라미터로 받아 메소드를 선택 할 수 있음
+        return calculation.calculate(first, second);
     }
 }
