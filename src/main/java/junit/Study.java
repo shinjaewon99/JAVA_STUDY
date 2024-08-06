34eef5d990c553c16d936c590fa82cc7c7f2c220
shinjaewon99 <tlswodnjs99@naver.com>
2024-08-06 23:17:39 +0900

    feat: 예외 테스트를 위한 코드 작성

--- src/main/java/junit/Study.java
+++ src/main/java/junit/Study.java
@@ -6,6 +6,16 @@ public class Study {
 
     private int limit;
 
+    public Study() {
+    }
+
+    public Study(final int limit) {
+        if (limit < 0) {
+            throw new IllegalArgumentException("limit은 0보다 커야 한다.");
+        }
+        this.limit = limit;
+    }
+
     public StudyStatus getStatus() {
         return this.studyStatus;
     }
